6bac31af8483c79dcf0fae332815971d595d582a
moT01 <20648924+moT01@users.noreply.github.com>
2020-10-23 12:18:02 -0500

    INIT

---
+++ insert_data.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
